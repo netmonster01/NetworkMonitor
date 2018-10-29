8cf40bfd44991c247777b6b12375b735cd531e53
netmonster01 <netmonster_01@yahoo.com>
2018-10-29 15:42:59 -0500

    little update

--- NetworkMonitorApi/ClientApp/src/app/admin/blog/images/images.component.ts
+++ NetworkMonitorApi/ClientApp/src/app/admin/blog/images/images.component.ts
@@ -13,10 +13,12 @@ export class ImagesComponent implements OnInit {
   formGroup = this.fb.group({
     file: [null, Validators.required]
   });
+
+
   constructor(private blogService: BlogService, private fb: FormBuilder) { }
 
   selectedFile: File;
-
+  fd: FormData;
   onFileSelected(event) {
     this.selectedFile = <File>event.target.files[0];
     console.log(this.selectedFile);
@@ -25,13 +27,13 @@ export class ImagesComponent implements OnInit {
   }
 
   uploadFile() {
-    const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
+   
+    this.fd.append('image', this.selectedFile, this.selectedFile.name);
     //let blogImage = new BlogImage {
     //  Image: this.selectedFile
     //}
     //// call service.
-    this.blogService.uploadIamge(fd);
+    this.blogService.uploadIamge(this.fd);
   }
 
   ngOnInit() {
